ca3019a1e5cf273a8734960c189dc8e56448d3a4
ef777 <efn444@gmail.com>
2022-10-01 14:46:01 +0300

    destek41

--- is/forms.py
+++ is/forms.py
@@ -17,7 +17,6 @@ class NewUserForm(UserCreationForm):
 		fields = ("username", "password1", "password2")
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
 		if commit:
 			user.save()
 		return user
